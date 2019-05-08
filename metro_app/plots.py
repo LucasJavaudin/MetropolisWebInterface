58823486a1c6026276badff3a47fa13592d3e358
LucasJavaudin <lucas.javaudin@gmail.com>
2019-05-08 20:08:19 +0200

    add matplotlib configdir

--- metro_app/plots.py
+++ metro_app/plots.py
@@ -1,3 +1,10 @@
+import os
+
+# Set matplotlib config directory.
+mplconfigdir = '/home/metropolis/matplotlib'
+if os.path.isdir(mplconfigdir):
+    os.environ['MPLCONFIGDIR'] = mplconfigdir
+
 import numpy as np
 import matplotlib.pyplot as plt
 
