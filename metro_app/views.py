93622789c0145e2993492dce48eafb8fa1ae8802
LucasJavaudin <lucas.javaudin@gmail.com>
2020-09-28 18:18:14 +0200

    comment unused views

--- metro_app/views.py
+++ metro_app/views.py
@@ -1493,55 +1493,55 @@ def pricing_view(request, simulation):
     return TollListView.as_view()(request, simulation=simulation, )
 
 
-@owner_required
-def pricing_edit(request, simulation):
-    # Get all pricing policies for this usertype.
-    policies = get_query('policy', simulation)
-    policies = policies.filter(usertype=demandsegment.usertype)
-    tolls = policies.filter(type='PRICING')
-    # Get all links of the network.
-    links = get_query('link', simulation)
-    # Get all LinkSelection of the network.
-    locations = LinkSelection.objects.filter(
-        network=simulation.scenario.supply.network
-    )
-    """View to edit the tolls."""
-    # Create a formset to edit the objects.
-    formset = PolicyFormSet
-    context = {
-        'simulation': simulation,
-        'demandsegment': demandsegment,
-        'tolls': tolls,
-        'links': links,
-        'locations': locations,
-        'formset': formset,
-    }
-    return render(request, 'metro_app/pricing_edit.html', context)
+# @owner_required
+# def pricing_edit(request, simulation):
+    # # Get all pricing policies for this usertype.
+    # policies = get_query('policy', simulation)
+    # policies = policies.filter(usertype=demandsegment.usertype)
+    # tolls = policies.filter(type='PRICING')
+    # # Get all links of the network.
+    # links = get_query('link', simulation)
+    # # Get all LinkSelection of the network.
+    # locations = LinkSelection.objects.filter(
+        # network=simulation.scenario.supply.network
+    # )
+    # """View to edit the tolls."""
+    # # Create a formset to edit the objects.
+    # formset = PolicyFormSet
+    # context = {
+        # 'simulation': simulation,
+        # 'demandsegment': demandsegment,
+        # 'tolls': tolls,
+        # 'links': links,
+        # 'locations': locations,
+        # 'formset': formset,
+    # }
+    # return render(request, 'metro_app/pricing_edit.html', context)
 
 
-@require_POST
-@owner_required
-def pricing_save(request, simulation):
-    """View to save the tolls of an user type."""
-    # Retrieve the formset from the POST data.
-    formset = PolicyFormSet(request.POST)
-    if formset.is_valid():
-        # Save the formset (updated values and newly created objects).
-        formset.save()
-        simulation.has_changed = True
-        simulation.save()
-    else:
-        # Redirect to a page with the errors.
-        context = {
-            'simulation': simulation,
-            'demandsegment': demandsegment,
-            'form': formset,
-        }
-        return render(request, 'metro_app/errors.html', context)
+# @require_POST
+# @owner_required
+# def pricing_save(request, simulation):
+    # """View to save the tolls of an user type."""
+    # # Retrieve the formset from the POST data.
+    # formset = PolicyFormSet(request.POST)
+    # if formset.is_valid():
+        # # Save the formset (updated values and newly created objects).
+        # formset.save()
+        # simulation.has_changed = True
+        # simulation.save()
+    # else:
+        # # Redirect to a page with the errors.
+        # context = {
+            # 'simulation': simulation,
+            # 'demandsegment': demandsegment,
+            # 'form': formset,
+        # }
+        # return render(request, 'metro_app/errors.html', context)
 
-    return HttpResponseRedirect(reverse(
-        'metro:pricing_edit', args=(simulation.id, demandsegment.id,)
-    ))
+    # return HttpResponseRedirect(reverse(
+        # 'metro:pricing_edit', args=(simulation.id, demandsegment.id,)
+    # ))
 
 
 @public_required
