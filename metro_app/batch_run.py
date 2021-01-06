312c0bd5168cbb5f14f7ece560663c8863ec9612
Shubham Dhingra <54913662+ShubhamDhingra96@users.noreply.github.com>
2021-01-06 10:07:35 -0500

    Update batch_run.py

--- metro_app/batch_run.py
+++ metro_app/batch_run.py
@@ -1,14 +1,9 @@
-#Implemented a External Script for the Batch Process on 20 December 2020 By Shubham
+"""Implemented a External Script for the Batch Process on 20 December 2020 By Shubham"""
 
 import os
 import sys
-import json
-from shutil import copyfile
-
 import django
 from django.utils import timezone
-from django.conf import settings
-from django.db.models import Sum
 
 # Load the django website.
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -25,46 +20,45 @@ print('Starting script...')
 
 # Read argument of the script call.
 try:
-    run_id = int(sys.argv[1])
+    batch_id = int(sys.argv[1])
 except IndexError:
     raise SystemExit('MetroArgError: This script must be executed with the id '
                      + 'of the SimulationRun has an argument.')
 
 
 try:
-    batch_id = int(sys.argv[1])
     batch = models.Batch.objects.get(pk=batch_id)
     batch.batchrun_set.all()
-    sim = batch.simulation
+    simulation = batch.simulation
 
     for batch_run in batch.batchrun_set.all():
         if batch_run.centroid_file:
-            functions.object_import_function(batch_run.centroid_file.file, sim,  "centroid")
+            functions.object_import_function(batch_run.centroid_file.file, simulation,  "centroid")
 
         if batch_run.crossing_file:
-            functions.object_import_function( batch_run.crossing_file.file, sim, "crossing")
+            functions.object_import_function( batch_run.crossing_file.file, simulation, "crossing")
 
         if batch_run.link_file:
-            functions.object_import_function(batch_run.link_file.file, sim, "link")
-
-        if batch_run.pricing_file:
-            functions.pricing_import_function(batch_run.pricing_file.file, sim)
+            functions.object_import_function(batch_run.link_file.file, simulation, "link")
 
         if batch_run.public_transit_file:
-            functions.public_transit_import_function(batch_run.public_transit_file.file, sim)
+            functions.public_transit_import_function(batch_run.public_transit_file.file, simulation)
 
         if batch_run.traveler_file:
-            functions.traveler_zip_file(sim, batch_run.traveler_file)
+            functions.traveler_zip_file(simulation, batch_run.traveler_file)
+
+        if batch_run.pricing_file:
+            functions.pricing_import_function(batch_run.pricing_file.file, simulation)
 
         if batch_run.function_file:
-            functions.object_import_function(batch_run.function_file.file, sim, "function")
+            functions.object_import_function(batch_run.function_file.file, simulation, "function")
 
         if batch_run.zip_file:
-            functions.simulation_import(sim, batch_run.zip_file)
+            functions.simulation_import(simulation, batch_run.zip_file)
 
-        run = models.SimulationRun(name=batch_run.name, simulation=sim)
+        run = models.SimulationRun(name=batch_run.name, simulation=simulation)
         run.save()
-        batch_run.run = run
+        batch_run.run
         batch_run.save()
         functions.run_simulation(run, background=False)
 
@@ -77,3 +71,4 @@ except models.BatchRun.DoesNotExist:
 
 batch.end_time = timezone.now
 batch.status = "Finished"
+batch.save()
